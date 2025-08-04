233c05358f4db954bc93b45a7222fc2184269b12
Totoro <huangtong.ht@qq.com>
2025-08-04 16:24:05 +0800

    feat: update size

--- src/works/constants.tsx
+++ src/works/constants.tsx
@@ -19,7 +19,7 @@ export const useWorks = (): IWork[] => {
     {
       id: "monthly-calendar-gen",
       name: "月历 CJ-MVTCP001",
-      desc: "图的尺寸是：153mm * 225mm",
+      desc: "图的印刷尺寸是：153mm * 225mm，按 DPI 300 印刷，像素尺寸至少 1807 × 2657",
       content: CJ_MVTCP001,
       illustration: "/assets/CJ-MVTCP001_illustration.png",
       tips: [
@@ -32,7 +32,7 @@ export const useWorks = (): IWork[] => {
     {
       id: "monthly-calendar-gen-002",
       name: "月历 CJ-MVTCP002",
-      desc: "图的尺寸是：153mm * 225mm",
+      desc: "图的印刷尺寸是：145mm * 215mm，按 DPI 300 印刷，像素尺寸至少 1713 × 2540",
       content: CJ_MVTCP002,
       illustration: "/assets/CJ-MVTCP002_illustration.png",
       tips: [
